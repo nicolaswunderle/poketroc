68801ff4d8573fe39995e539167e8a52c9d822b0
Nicolas Wunderle <nicolas.wunderle@heig-vd.ch>
2023-12-01 18:21:01 +0100

    création du dresseur qui s'authetifie

--- spec/utils.js
+++ spec/utils.js
@@ -1,7 +1,12 @@
+import jwt from "jsonwebtoken"
+import { promisify } from "util";
+import { jwtSecret } from "../config.js";
 import Dresseur from "../models/dresseur.js";
 import Message from "../models/message.js";
 import Echange from "../models/echange.js";
 import Carte from "../models/carte.js";
+
+const signJwt = promisify(jwt.sign);
 
 export const cleanUpDatabase = async function () {
   await Promise.all([
@@ -11,3 +16,13 @@ export const cleanUpDatabase = async function () {
     Carte.deleteMany(),
   ]);
 };
+
+export function generateValidJwt(dresseur) {
+  // Generate a valid JWT which expires in 7 days.
+  console.log(dresseur._id.toString())
+  const payload = { 
+    sub: dresseur._id.toString(),
+    exp: Math.floor(Date.now() / 1000) + 7 * 24 * 3600,
+  };
+  return signJwt(payload, jwtSecret);
+}
